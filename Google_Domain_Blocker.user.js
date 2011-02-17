611a50fc357f5cf7befc026943da9dd86a5c3407
Paul J <pjobson@gmail.com>
2011-02-17 13:38:00 +0000

    Minor color update for yes/no confirmation.

--- Google_Domain_Blocker.user.js
+++ Google_Domain_Blocker.user.js
@@ -4,10 +4,9 @@
 // @description    Blocks irrelevant and spam domains.
 // @include        http://*.google.*/*
 // @include        https://*.google.*/*
-// @exclude        http://*.google.*/*&tbs=shop*
 // @exclude        http://mail.google.com/*
 // @exclude        https://mail.google.com/*
-// @require        https://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js
+// @require        http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js
 // @require        http://sizzlemctwizzle.com/updater.php?id=33156
 // ==/UserScript==
 
